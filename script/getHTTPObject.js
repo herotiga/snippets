dd5082258510062be4e2f0aa3e5a777ac355bd83
wangwei <herotiga@vip.qq.com>
2020-04-17 00:38:15 +0800

    auto committed by shell script

--- script/getHTTPObject.js
+++ script/getHTTPObject.js
@@ -1,5 +1,5 @@
 function getHTTPObject() {
-    if (typeof XMLHttpRequest == 'undefines')
+    if (typeof XMLHttpRequest == 'undefined')
         XMLHttpRequest = function () {
             try {return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
             catch (e) {}
